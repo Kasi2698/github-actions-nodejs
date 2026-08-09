01be0aa73db75c629f46d0bfc9f72c0d65d73fe6
Kasiniventhan <kasi@gmail.com>
2026-08-09 14:22:28 +0000

    Initial Node.js application

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from Node.js GitHub Actions!");
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "UP"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
